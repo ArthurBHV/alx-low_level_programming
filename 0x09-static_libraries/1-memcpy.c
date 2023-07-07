a030d7760d75197d5b6c032d80c15913ec8d2be2
ArthurBHV <arthurngqeku@aol.com>
2023-07-07 07:37:06 -0700

    Static Library

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory
- * @dest: destination string
- * @src: source string
- * @n: number of bytes copied
- * Return: pointer to destination
+ * _memcpy - Entry point
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0 (Success)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *ptr = dest;
+	unsigned int i;
 
-	while (n--)
-		*dest++ = *src++;
-	return (ptr);
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+
+	return (dest);
 }
